96bf16ef4b2dbe2a5f63a38c1987a8070b55cb4e
khairul24 <khairul_makirin@yahoo.com>
2020-09-08 17:48:58 +0700

    finished integrate playfair cipher with UI

--- src/components/PlayfairCipher.vue
+++ src/components/PlayfairCipher.vue
@@ -1,11 +1,68 @@
 <template>
-  <h1>playfair</h1>
+  <div class="playfair-cipher">
+    <h3>Playfair Cipher</h3>
+    <div class="keystring-container">
+      <p>Cipher Key:</p>
+      <input
+        v-model="keystring"
+        class="keystring-input"
+        type="text"
+        placeholder="sayasukakriptografi"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from "vuex";
+import { removeWhiteSpace } from "@/utils/preprocessor";
+import {
+  PlayfairCipherEncrypt,
+  PlayfairCipherDecrypt
+} from "@/utils/playfair_cipher";
+
 export default {
-  name: "PlayfairCipher"
+  name: "PlayfairCipher",
+  data() {
+    return {
+      keystring: "",
+    }
+  },
+  computed: {
+    ...mapGetters(['isProcessing', 'isEncrypt'])
+  },
+  methods: {
+    ...mapMutations(["setPlaintext", "setCiphertext", "endProcessing"]),
+    ...mapGetters(["getPlaintext", "getCiphertext"])
+  },
+  watch: {
+    isProcessing(newVal) {
+      if (newVal) {
+        if (this.isEncrypt) {
+          this.setCiphertext(PlayfairCipherEncrypt(this.getPlaintext(), removeWhiteSpace(this.keystring)));
+        } else {
+          this.setPlaintext(PlayfairCipherDecrypt(this.getCiphertext(), removeWhiteSpace(this.keystring)));
+        }
+
+        this.endProcessing()
+      }
+    }
+  }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.playfair-cipher {
+  .keystring-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-top: 10px;
+
+    .keystring-input {
+      margin-left: 10px;
+      flex-grow: 1;
+    }
+  }
+}
+</style>
